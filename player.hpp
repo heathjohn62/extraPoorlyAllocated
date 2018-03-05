e0c9214c15079848505ea0971ba3de593bcbe1de
heathjohn62 <heathjohn62@gmail.com>
2018-03-04 20:14:12 -0800

    Small Update

--- player.hpp
+++ player.hpp
@@ -14,7 +14,15 @@ public:
     Board board;
     Side player_side;
     Side opponent_side;
-
+    int board_weights[8][8]= 
+    {{20, -3, 1, 1, 1, 1, -3, 20},
+     {-3, -5, 1, 1, 1, 1, -5, -3},
+     { 4,  1, 1, 1, 1, 1,  1,  4},
+     { 4,  1, 1, 1, 1, 1,  1,  4},
+     { 4,  1, 1, 1, 1, 1,  1,  4}, 
+     { 4,  1, 1, 1, 1, 1,  1,  4},
+     {-3, -5, 1, 1, 1, 1, -5, -3},
+     {20, -3, 1, 1, 1, 1, -3, 20}};
     Move *doMove(Move *opponentsMove, int msLeft);
 
     // Flag to tell if the player is running within the test_minimax context
